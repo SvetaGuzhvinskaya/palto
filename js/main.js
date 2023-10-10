42614e7737fb33a63412492c453243a813a3a4d4
Svetlana Gughvinskaya <blacksam13@gmail.com>
2023-10-10 09:48:53 +0300

    commit6

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    loop:true,
+    nav:false,
+    dots:false,
+    margin:10,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },            
+        960:{
+            items:3
+        },
+        1200:{
+            items:3
+        }
+    }
+});
+owl.on('mousewheel', '.owl-stage', function (e) {
+    if (e.deltaY>0) {
+        owl.trigger('next.owl');
+    } else {
+        owl.trigger('prev.owl');
+    }
+    e.preventDefault();
+});
+
 var visibilitychangeLogEl = document.querySelector('.vector-line');
 anime({
   targets: '.vector-line path',
@@ -13,23 +43,25 @@ let screenBootm = document.querySelector('.screen-bottom')
 screenBootm.addEventListener("mouseover", () => {
   screenBootm.classList.add('animation-started')
 })
-Array.prototype.forEach.call(
-  document.querySelectorAll('.js-scroll'),
-  (el) => new SimpleBar(el)
-);
+// Array.prototype.forEach.call(
+//   document.querySelectorAll('.js-scroll'),
+//   (el) => new SimpleBar(el)
+// );
 
 const mainPage = document.querySelector('.main-page')
 const btns = document.querySelectorAll('.js-btn')
 const screen = document.querySelectorAll('.js-screen')
 const btnsBack = document.querySelectorAll('.js-btn-back')
 
-btns.forEach((btn, btnInd) => {
+btns.forEach((btn) => {
   btn.addEventListener('click', function (e) {
     e.preventDefault()
     mainPage.classList.add('hidden')
-    screen.forEach((item, itemInd) => {
+    let btnsData = btn.getAttribute('data-screen')
+    screen.forEach((item) => {
       item.classList.add('hidden')
-      if(btnInd === itemInd) {
+      let screenId = item.getAttribute("id")
+      if(btnsData === screenId) {
         item.classList.remove('hidden')
       }
       
@@ -53,35 +85,7 @@ btnsBack.forEach((btn) => {
 
 const butterfly = document.querySelector('.js-butterfly')
 const arm = document.querySelector('.js-arm')
-
-
-// var leftItem = document.getElementById('item0'),
-//     rightItem = document.getElementById('item1');
-
-// ;(function(){
-
-//   var throttle = function(type, name, obj){
-//     var obj = obj || window;
-//     var running = false;
-//     var func = function(){
-//       if (running){ return; }
-//       running = true;
-//       requestAnimationFrame(function(){
-//         obj.dispatchEvent(new CustomEvent(name));
-//         running = false;
-//       });
-//     };
-//     obj.addEventListener(type, func);
-//   };
-  
-//   throttle("scroll", "optimizedScroll");
-// })();
-
-// window.addEventListener("optimizedScroll", function(){
-  
-//   leftItem.style.transform = "rotate(-" + window.pageYOffset + "deg)";
-//   rightItem.style.transform = "rotate(" + window.pageYOffset + "deg)";
-// })
+const textParalax = document.querySelector('.js-plax')
 
 // load
 const jsLoad = document.querySelector('.js-load')
@@ -99,16 +103,48 @@ window.onload = function() {
   
 }
 
-window.addEventListener('scroll', function() {
-  // if(window.scrollY < 100) {
-  //   butterfly.style.transform = `translate(${window.scrollY}px, -${window.scrollY - 10}px)rotate(-${window.scrollY - 5}deg)`
-  // } if(window.scrollY < 200 && window.scrollY > 100) {
-  //   butterfly.style.transform = `translate(${window.scrollY}px, -${window.scrollY - 10}px)rotate(-${window.scrollY - 170}deg)`
-  // } if(window.scrollY < 300 && window.scrollY > 200) {
-  //   butterfly.style.transform = `translate(${window.scrollY}px, -${window.scrollY - 230}px)rotate(-${window.scrollY - 410}deg)`
-  // }
+// scroll
+;(function(){
 
+  var throttle = function(type, name, obj){
+    var obj = obj || window;
+    var running = false;
+    var func = function(){
+      if (running){ return; }
+      running = true;
+      requestAnimationFrame(function(){
+        obj.dispatchEvent(new CustomEvent(name));
+        running = false;
+      });
+    };
+    obj.addEventListener(type, func);
+  };
+  
+  throttle("scroll", "optimizedScroll");
+})();
+
+window.addEventListener("optimizedScroll", function(){
+  let offsetTop = document.querySelector('.screen-top').offsetTop - 100
+  let offsetMiddle = document.querySelector('.screen-middle').offsetTop - 300
   let offsetFooter = document.querySelector('.screen-footer').offsetTop - 600
+
+  if(window.scrollY >= 1) {
+    butterfly.style.transform = `translate(${window.scrollY}px, -${window.scrollY - 5}px)rotate(-${window.scrollY - 2}deg)`
+  } if(window.scrollY >= 80 && window.scrollY < 200) {
+    butterfly.style.transform = `translate(${window.scrollY}px, -${window.scrollY}px)rotate(-${window.scrollY / 2}deg)`
+  } if(window.scrollY >= 200 && window.scrollY < 400) {
+    butterfly.style.transform = `translate(${window.scrollY}px, -200px)rotate(-20deg)`
+  } if(window.scrollY <= 0) {
+    butterfly.style.transform = `translate(0, 0)rotate(0)`
+  }
+  
+  
+
+  if(window.scrollY > offsetMiddle){
+    if(((window.scrollY - offsetMiddle) / 2) <= textParalax.offsetHeight){
+      textParalax.style.transform = `translateY(${(window.scrollY - offsetMiddle) / 2}px)`
+    }
+  }
 
   if(window.scrollY > offsetFooter){
     arm.style.transform = `translateX(-${window.scrollY - offsetFooter}px)`
